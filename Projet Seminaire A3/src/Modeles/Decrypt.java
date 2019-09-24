a17acc916381a7129b841812b36c463d5e753951
JeanAkaPascal <33148890+JeanAkaPascal@users.noreply.github.com>
2019-09-24 15:20:52 +0200

    commit decrypt

--- Projet Seminaire A3/src/Modeles/Decrypt.java
+++ Projet Seminaire A3/src/Modeles/Decrypt.java
@@ -2,7 +2,33 @@ package Modeles;
 
 public class Decrypt {
 
-	String decrypt(String data, String key) {
-		return "string";
+	public String decrypt(String inputString, String key) 
+	{
+		StringBuilder sb1;
+		char c1;
+		char c2;
+		char c3;
+		int i;
+		int ii;
+		
+		ii=0;
+		sb1= new StringBuilder();
+		
+		for(i=0;i<inputString.length();i++)
+		{
+			c1=inputString.charAt(i);
+			c2=key.charAt(ii);
+
+			c3 = (char)(c1^c2);
+			sb1.append(c3);
+			
+            ii++;
+            if(ii == key.length())
+            {
+                ii = 0;
+            }
+		}
+		return sb1.toString();
 	}
+
 }
