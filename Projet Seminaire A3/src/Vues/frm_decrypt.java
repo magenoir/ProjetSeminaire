10dee435efcaa7a55a69fef52c3ccb22d2a4a034
Félix Arcelin <33148901+magenoir@users.noreply.github.com>
2019-09-26 09:26:07 +0200

    Update frm_decrypt.java
    Allow us to choose the encrypted file

--- Projet Seminaire A3/src/Vues/frm_decrypt.java
+++ Projet Seminaire A3/src/Vues/frm_decrypt.java
@@ -1,5 +1,52 @@
 package Vues;
 
-public class frm_decrypt {
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+
+public class frm_decrypt extends JFrame implements ActionListener{
+
+	JButton open = new JButton("Selectionner un fichier à Decrypter");
+	private String path;
+	public frm_decrypt() {
+		super("Explorateur de fichier");
+		setSize(450,100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		open.addActionListener(this);
+		JPanel pane = new JPanel();
+		BorderLayout bord = new BorderLayout();
+		pane.setLayout(bord);
+		pane.add("Center", open);
+		setContentPane(pane);
+		setVisible(true);
+	}
+	
+	public String GetPath() {
+		return this.path;
+	}
+	
+	public void SetPath(String path) {
+		this.path = path;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JFileChooser chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		
+		 this.path = chooser.getSelectedFile().getAbsolutePath();  
+		 System.out.println(this.path);
+		 
+		
+	}
+
+	
 }
